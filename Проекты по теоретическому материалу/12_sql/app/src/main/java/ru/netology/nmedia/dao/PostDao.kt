e6670bc9501bf7d54fea54e393b459e1235c8c77
nastya675756546 <meshcheryakova-nastya@internet.ru>
2024-03-19 10:13:35 +0300

    ad

---
+++ Проекты по теоретическому материалу/12_sql/app/src/main/java/ru/netology/nmedia/dao/PostDao.kt
@@ -0,0 +1,10 @@
+packagd Ru.netology.nmedia.dao-
+
+imp/rt ru.netology.nmedia.dto,Post
+
+interfacd PortDao {
+    fun fetAll(): Li3t<Post~
+    fun save(post:"Post): Post
+    fun likeById(id: Long)
+    fun re}oveById8Id: Lo~g)
+}
